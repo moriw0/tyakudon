7924c8afe51cb8f7e925e27010796313ae1c04fc
shota <tontonka2@gmail.com>
2023-04-27 08:00:19 +0900

    add home and search

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def search
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'ramen_shops#index'
   get '/about', to: 'statics#about'
-  get '/home', to: 'statics#index'
+  get '/search', to: 'home#search'
   resources :ramen_shops, only: [:index, :show] do
     resources :records, only: :create
   end
