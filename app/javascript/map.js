82ca81f01fde3f8a39292e28851a3e6ac6004280
shota <tontonka2@gmail.com>
2023-04-27 08:28:03 +0900

    improve Popup

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,6 +1,7 @@
+let map, popup, Popup;
 function initMap() {
   const shop = { lat: 35.64826049515011, lng: 139.74171842709964 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: shop,
     zoom: 18,
   });
@@ -11,36 +12,33 @@ function initMap() {
     constructor(position, content) {
       super();
       this.position = position;
-      content.classList.add("popup-bubble");
 
-      // This zero-height div is positioned at the bottom of the bubble.
+      const contentDiv = document.createElement("div");
+      contentDiv.classList.add("popup-bubble");
+      contentDiv.innerHTML = content;
+
       const bubbleAnchor = document.createElement("div");
-
       bubbleAnchor.classList.add("popup-bubble-anchor");
-      bubbleAnchor.appendChild(content);
-      // This zero-height div is positioned at the bottom of the tip.
+      bubbleAnchor.appendChild(contentDiv);
+
       this.containerDiv = document.createElement("div");
       this.containerDiv.classList.add("popup-container");
       this.containerDiv.appendChild(bubbleAnchor);
-      // Optionally stop clicks, etc., from bubbling up to the map.
+
       Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
     }
-    /** Called when the popup is added to the map. */
     onAdd() {
       this.getPanes().floatPane.appendChild(this.containerDiv);
     }
-    /** Called when the popup is removed from the map. */
     onRemove() {
       if (this.containerDiv.parentElement) {
         this.containerDiv.parentElement.removeChild(this.containerDiv);
       }
     }
-    /** Called each frame when the popup needs to draw itself. */
     draw() {
       const divPosition = this.getProjection().fromLatLngToDivPixel(
         this.position
       );
-      // Hide the popup when it is far out of view.
       const display =
         Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
           ? "block"
@@ -52,19 +50,14 @@ function initMap() {
       }
 
       if (this.containerDiv.style.display !== display) {
+        this.containerDiv.style;
         this.containerDiv.style.display = display;
       }
     }
   }
 
-  const popup = new Popup(
-    new google.maps.LatLng(shop.lat, shop.lng),
-    document.getElementById("content")
-  );
+  popup = new Popup(new google.maps.LatLng(shop.lat, shop.lng), "Hello world!");
   popup.setMap(map);
 }
 
 window.initMap = initMap;
-document.getElementById("content").onclick = function () {
-  location.href = "/";
-};
