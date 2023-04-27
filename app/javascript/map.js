7649a4f275585975beb726fdb1b9c2516bc186f5
shota <tontonka2@gmail.com>
2023-04-27 21:28:02 +0900

    apply shop url

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -3,36 +3,45 @@ function initMap() {
   class Popup extends google.maps.OverlayView {
     position;
     containerDiv;
-    constructor(position, content) {
+    constructor(position, content, url) {
       super();
       this.position = position;
 
-      const contentDiv = document.createElement("div");
-      contentDiv.classList.add("popup-bubble");
-      contentDiv.innerHTML = content;
+      // Create the anchor tag with content and add the popup-bubble class
+      const anchor = document.createElement("a");
+      anchor.href = url;
+      anchor.classList.add("popup-bubble");
+      anchor.innerHTML = content;
 
+      // This zero-height div is positioned at the bottom of the bubble.
       const bubbleAnchor = document.createElement("div");
       bubbleAnchor.classList.add("popup-bubble-anchor");
-      bubbleAnchor.appendChild(contentDiv);
+      bubbleAnchor.appendChild(anchor);
 
+      // This zero-height div is positioned at the bottom of the tip.
       this.containerDiv = document.createElement("div");
       this.containerDiv.classList.add("popup-container");
       this.containerDiv.appendChild(bubbleAnchor);
 
+      // Optionally stop clicks, etc., from bubbling up to the map.
       Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
     }
+    /** Called when the popup is added to the map. */
     onAdd() {
       this.getPanes().floatPane.appendChild(this.containerDiv);
     }
+    /** Called when the popup is removed from the map. */
     onRemove() {
       if (this.containerDiv.parentElement) {
         this.containerDiv.parentElement.removeChild(this.containerDiv);
       }
     }
+    /** Called each frame when the popup needs to draw itself. */
     draw() {
       const divPosition = this.getProjection().fromLatLngToDivPixel(
         this.position
       );
+      // Hide the popup when it is far out of view.
       const display =
         Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
           ? "block"
@@ -44,12 +53,10 @@ function initMap() {
       }
 
       if (this.containerDiv.style.display !== display) {
-        this.containerDiv.style;
         this.containerDiv.style.display = display;
       }
     }
   }
-
   // 現在地の緯度経度情報を取得
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -81,21 +88,13 @@ function initMap() {
               var distance = getDistance(currentLocation, shopLocation);
               // 距離が100m以内の場合、マーカーを地図上に表示
               if (distance <= 500) {
+                const url = "/ramen_shops/" + data[i].id;
                 popup = new Popup(
                   new google.maps.LatLng(shopLocation.lat, shopLocation.lng),
-                  data[i].name
+                  data[i].name,
+                  url
                 );
                 popup.setMap(map);
-                // マーカーがクリックされたときの処理
-                google.maps.event.addListener(
-                  popup,
-                  "click",
-                  (function (data) {
-                    return function () {
-                      window.location.href = "/ramen_shops/" + data.id;
-                    };
-                  })(data[i])
-                );
               }
             }
           })
