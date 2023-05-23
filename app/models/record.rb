e445d6ff97a18090e9048101e9add626f4db5038
shota <tontonka2@gmail.com>
2023-05-24 05:29:53 +0900

    add relation and upgrade model

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,7 @@
 class Record < ApplicationRecord
   belongs_to :ramen_shop
+  has_many :line_statuses
+  accepts_nested_attributes_for :line_statuses
 
   def calculate_wait_time!
     self.ended_at = Time.current
