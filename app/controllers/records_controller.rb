1fa8957f3a020d1247dca1f936e69462eefe3cb4
shota <tontonka2@gmail.com>
2023-05-26 07:59:03 +0900

    create set_ramen_shop and set_ramen_shop_record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,25 +1,23 @@
 class RecordsController < ApplicationController
+  before_action :set_ramen_shop
+  before_action :set_ramen_shop_record, only: %i[measure edit update]
+
   def new
-    @ramen_shop = RamenShop.find(params[:ramen_shop_id])
     @ramen_shop_record = @ramen_shop.records.build
     @ramen_shop_record.line_statuses.build
   end
 
   def create
-    ramen_shop = RamenShop.find(params[:ramen_shop_id])
     ramen_shop_record = Record.new(record_param)
 
     if ramen_shop_record.save
-      redirect_to ramen_shop_record_measure_path(ramen_shop, ramen_shop_record)
+      redirect_to measure_ramen_shop_record_path(@ramen_shop, ramen_shop_record)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def measure
-    @ramen_shop = RamenShop.find(params[:ramen_shop_id])
-    @ramen_shop_record = Record.find(params[:id])
-
     if @ramen_shop_record.update(started_at: Time.current)
       flash.notice = "セツゾクしました"
     else
@@ -28,23 +26,28 @@ class RecordsController < ApplicationController
   end
 
   def edit
-    @ramen_shop = RamenShop.find(params[:ramen_shop_id])
-    @ramen_shop_record = Record.find(params[:id])
   end
 
   def update
-    ramen_shop_record = Record.find(params[:id])
-    ramen_shop_record.assign_attributes(record_param)
-    ramen_shop_record.calculate_wait_time!
+    @ramen_shop_record.assign_attributes(record_param)
+    @ramen_shop_record.calculate_wait_time!
 
-    if ramen_shop_record.save
-      redirect_to ramen_shop_path(ramen_shop_record.ramen_shop), notice: 'ちゃくどんレコードを登録しました'
+    if @ramen_shop_record.save
+      redirect_to ramen_shop_path(@ramen_shop_record.ramen_shop), notice: 'ちゃくどんレコードを登録しました'
     else
       render :new
     end
   end
 
   private
+
+  def set_ramen_shop
+    @ramen_shop = RamenShop.find(params[:ramen_shop_id])
+  end
+
+  def set_ramen_shop_record
+    @ramen_shop_record = Record.find(params[:id])
+  end
 
   def record_param
     params.require(:record).permit(:ramen_shop_id, :started_at, :ended_at, line_statuses_attributes: [:line_number, :line_type])
