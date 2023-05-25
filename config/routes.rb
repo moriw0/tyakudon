0844768063f8f47be3e303ccc9d46ef13d057b8d
shota <tontonka2@gmail.com>
2023-05-25 12:42:49 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   get '/about', to: 'statics#about'
   get '/search', to: 'home#search'
   get '/near_shops', to: 'ramen_shops#near_shops'
+
   resources :ramen_shops, only: [:index, :show] do
-    resources :records, only: [:new, :create]
+    resources :records, only: [:new, :create, :edit, :update] do
+      get 'measure', to: 'records#measure'
+
+      resources :line_statuses, only: [:index, :new, :create] do
+      end
+    end
   end
 end
