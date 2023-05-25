6a238833923e1a96e8ed84bf98d5fa88cc4c9047
shota <tontonka2@gmail.com>
2023-05-25 12:49:10 +0900

    add connect flow

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,12 +1,36 @@
 class RecordsController < ApplicationController
   def new
     @ramen_shop = RamenShop.find(params[:ramen_shop_id])
-    @ramen_shop_record = @ramen_shop.records.build(
-      started_at: Time.current
-    )
+    @ramen_shop_record = @ramen_shop.records.build
+    @ramen_shop_record.line_statuses.build
+    # @ramen_shop_record = @ramen_shop.records.build(
+    #   started_at: Time.current
+    # )
   end
 
   def create
+    ramen_shop = RamenShop.find(params[:ramen_shop_id])
+    record = Record.new(record_param)
+
+    if record.save
+      redirect_to ramen_shop_record_measure_path(ramen_shop, record)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def measure
+    @ramen_shop = RamenShop.find(params[:ramen_shop_id])
+    @record = Record.find(params[:record_id])
+
+    if @record.update(started_at: Time.current)
+      flash.notice = "セツゾクしました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
     @record = Record.new(record_param).calculate_wait_time!
 
     if @record.save
@@ -19,6 +43,6 @@ class RecordsController < ApplicationController
   private
 
   def record_param
-    params.require(:record).permit(:ramen_shop_id, :started_at, :queue_number)
+    params.require(:record).permit(:ramen_shop_id, :started_at, line_statuses_attributes: [:line_number, :line_type])
   end
 end
