1318e78cca8bf2068afb30cc95190701c6d46949
shota <tontonka2@gmail.com>
2023-06-09 08:50:19 +0900

    modify line_statuse order

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
-bundle exec rails db:seed
+bundle exec rails db:reset
