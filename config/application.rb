c8bd84a262b19b253b30863b90e5102c58469002
shota <tontonka2@gmail.com>
2023-06-22 21:30:05 +0900

    generate setting

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Tyakudon
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false, view_specs: false, helper_specs: false, routing_specs: false
+      g.factory_bot false
+    end
   end
 end
