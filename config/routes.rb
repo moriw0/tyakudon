60afdced1b044ea4f2a95961cc6666d399ce9e38
shota <tontonka2@gmail.com>
2023-06-23 20:14:18 +0900

    modify signup route and add link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'home#index'
   get '/about', to: 'statics#about'
   get '/search', to: 'home#search'
   get '/near_shops', to: 'ramen_shops#near_shops'
+  get '/signup',  to: 'users#new'
 
   resources :ramen_shops, only: [:index, :show] do
     resources :records, only: [:show, :new, :create, :edit, :update], shallow: true do
