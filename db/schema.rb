04b3247ffb650b3c9f91a1576c3148279a311525
shota <tontonka2@gmail.com>
2023-06-23 20:17:10 +0900

    generate user model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_21_211816) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_111617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_21_211816) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ramen_shop_id"], name: "index_records_on_ramen_shop_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "line_statuses", "records"
