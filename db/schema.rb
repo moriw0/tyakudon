1865eca0d57dc72e26e93a72b45b82a7eac2925a
shota <tontonka2@gmail.com>
2023-06-24 15:19:06 +0900

    add index to users email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_111617) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_061308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_111617) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "line_statuses", "records"
