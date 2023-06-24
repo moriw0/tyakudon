fd6040ced34a87a54812bacb8922021cdf0c2aab
shota <tontonka2@gmail.com>
2023-06-24 15:11:28 +0900

    add validation for name, email and spec for them

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    name { 'Example User' }
+    email { 'user@example.com' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User do
+  it 'is valid with name and email' do
+    user = described_class.new(
+      name: 'Example User',
+      email: 'user@example.com'
+    )
+
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    new_user = build(:user, name: '    ')
+    new_user.valid?
+    expect(new_user.errors[:name]).to include('を入力してください')
+  end
+
+  it 'is invalid without an email' do
+    new_user = build(:user, email: '    ')
+    new_user.valid?
+    expect(new_user.errors[:email]).to include('を入力してください')
+  end
+
+  it 'is invalid without a too long name' do
+    new_user = build(:user, name: 'a' * 51)
+    new_user.valid?
+    expect(new_user.errors[:name]).to include('は50文字以内で入力してください')
+  end
+
+  it 'is invalid without a too long email' do
+    new_user = build(:user, email: "#{'a' * 244}@example.com")
+    new_user.valid?
+    expect(new_user.errors[:email]).to include('は255文字以内で入力してください')
+  end
+
+  it 'is valid with valid addresses' do
+    new_user = build(:user)
+    valid_addresses = %w[user@example.com USER@foo.COM
+                         A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      new_user.email = valid_address
+      expect(new_user).to be_valid
+    end
+  end
+
+  it 'is invalid with invalid addresses' do
+    new_user = build(:user)
+    invalid_addresses = %w[user@example,com
+                           user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com
+                           foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      new_user.email = invalid_address
+      expect(new_user).to be_invalid, "#{invalid_address.inspect}が無効ではありません"
+    end
+  end
+
+  it 'is invalid with duplicate address' do
+    new_user = create(:user)
+    duplicate_user = new_user.dup
+    duplicate_user.email = new_user.email.upcase
+    duplicate_user.valid?
+    expect(duplicate_user.errors[:email]).to include('はすでに存在します')
+  end
 end
