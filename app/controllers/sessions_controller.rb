86b26a1f121dfb80f0c70e8f37cd4754f36501b2
shota <tontonka2@gmail.com>
2023-06-28 18:03:40 +0900

    add handling if create action success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now.alert = 'ログインに失敗しました'
       render 'new', status: :unprocessable_entity
