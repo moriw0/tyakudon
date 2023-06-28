8fc7a1c8aee882522167c1592f389e0a5daff73d
shota <tontonka2@gmail.com>
2023-06-28 18:31:54 +0900

    activate hambuger

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.7/dist/esm/index.js"
 pin "popup"
