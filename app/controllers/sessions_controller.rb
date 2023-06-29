54fdc054696a26dda82dcc02b2cc5d5d9ccfc621
shota <tontonka2@gmail.com>
2023-06-30 06:08:53 +0900

    add spec for login with valid information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       reset_session
       log_in user
-      redirect_to user
+      redirect_to user, notice: 'ログインしました'
     else
       flash.now.alert = 'ログインに失敗しました'
       render 'new', status: :unprocessable_entity
--- spec/system/logins_spec.rb
+++ spec/system/logins_spec.rb
@@ -5,13 +5,25 @@ RSpec.describe "Logins", type: :system do
     driven_by(:rack_test)
   end
 
-  scenario 'login with invalid information' do
+  let(:user) { create(:user) }
+
+  scenario 'login with valid email and invalid password' do
     visit login_path
-    fill_in 'メールアドレス', with: 'no-user@example.com'
-    fill_in 'パスワード', with: 'foobar'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'invalid'
     click_button 'ログインする'
     expect(page).to have_content 'ログインに失敗しました'
     visit root_path
     expect(page).not_to have_content 'ログインに失敗しました'
+  end
+
+  scenario 'login with valid information' do
+    visit login_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログインする'
+    expect(page).to have_content 'ログインしました'
+    expect(page).to have_link 'プロフィール', href: user_path(user)
+    expect(page).to have_link 'ログアウト', href: logout_path
   end
 end
