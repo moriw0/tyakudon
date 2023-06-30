c42c66d3e10230a30837958c3a10e008e152cc3a
shota <tontonka2@gmail.com>
2023-07-01 06:55:14 +0900

    apply rubocop

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,17 +4,19 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+    return unless session[:user_id]
+
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
     !current_user.nil?
   end
 
+  # rubocop:disable Rails/HelperInstanceVariable
   def log_out
     reset_session
     @current_user = nil
   end
+  # rubocop:enable Rails/HelperInstanceVariable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get '/about', to: 'statics#about'
   get '/search', to: 'home#search'
   get '/near_shops', to: 'ramen_shops#near_shops'
-  get '/signup',  to: 'users#new'
-  get    "/login",   to: "sessions#new"
-  post   "/login",   to: "sessions#create"
-  delete "/logout",  to: "sessions#destroy"
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :ramen_shops, only: [:index, :show] do
     resources :records, only: [:show, :new, :create, :edit, :update], shallow: true do
@@ -14,6 +14,5 @@ Rails.application.routes.draw do
       resources :line_statuses
     end
   end
-
   resources :users
 end
