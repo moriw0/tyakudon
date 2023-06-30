7f6b0d60f9f43a6ac6b91bc8a746c08da0086114
shota <tontonka2@gmail.com>
2023-07-01 06:52:36 +0900

    add logout and spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_path, status: :see_other, notice: 'ログアウトしました'
   end
 end
--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Logins", type: :system do
+RSpec.describe 'Logins' do
   before do
     driven_by(:rack_test)
   end
@@ -14,10 +14,10 @@ RSpec.describe "Logins", type: :system do
     click_button 'ログインする'
     expect(page).to have_content 'ログインに失敗しました'
     visit root_path
-    expect(page).not_to have_content 'ログインに失敗しました'
+    expect(page).to_not have_content 'ログインに失敗しました'
   end
 
-  scenario 'login with valid information' do
+  scenario 'login with valid information followed by logout' do
     visit login_path
     fill_in 'メールアドレス', with: user.email
     fill_in 'パスワード', with: user.password
@@ -25,5 +25,9 @@ RSpec.describe "Logins", type: :system do
     expect(page).to have_content 'ログインしました'
     expect(page).to have_link 'プロフィール', href: user_path(user)
     expect(page).to have_link 'ログアウト', href: logout_path
+    click_link 'ログアウト'
+    expect(page).to have_content 'ログアウトしました'
+    expect(page).to have_link 'ログイン', href: login_path
+    expect(page).to_not have_link 'ログアウト', href: logout_path
   end
 end
