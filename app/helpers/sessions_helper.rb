0bb160a8c4b9e5426a52d219fd61b220997d4aea
shota <tontonka2@gmail.com>
2023-07-03 09:11:38 +0900

    return current_user if session_tokens match

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,7 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
 
   def remember(user)
@@ -11,7 +12,10 @@ module SessionsHelper
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
