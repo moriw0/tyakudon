77b6f1dcbf9374903663a0831b10b9b89f74f503
shota <tontonka2@gmail.com>
2023-07-25 08:33:54 +0900

    add favorite controller spec

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Favorites", type: :request do
-  let(:favorite) { create(:favorite) }
+  let(:user) { create(:user) }
+  let(:ramen_shop) { create(:ramen_shop) }
 
   describe 'POST /favorites #create' do
     context 'when not logged in' do
@@ -10,13 +11,51 @@ RSpec.describe "Favorites", type: :request do
         expect(response).to redirect_to login_path
       end
     end
+
+    context 'when logged in' do
+      before do
+        log_in_as user
+      end
+
+      it 'adds shop to favorite the standard way' do
+        expect {
+          post favorites_path, params: { ramen_shop_id: ramen_shop.id }
+        }.to change(Favorite, :count).by(1)
+      end
+
+      it 'adds shop to favorite with Hotwire' do
+        expect {
+          post favorites_path, params: { ramen_shop_id: ramen_shop.id }, as: :turbo_stream
+        }.to change(Favorite, :count).by(1)
+      end
+    end
   end
 
   describe 'DELETE /favorites/:id #destory' do
+    let!(:favorite) { create(:favorite, user: user, ramen_shop: ramen_shop) }
+
     context 'when not logged in' do
       it 'redirects to login_path' do
         delete favorite_path(favorite)
         expect(response).to redirect_to login_path
+      end
+    end
+
+    context 'when logged in' do
+      before do
+        log_in_as user
+      end
+
+      it 'remove shop from favorite the standard way' do
+        expect {
+          delete favorite_path(favorite)
+        }.to change(Favorite, :count).by(-1)
+      end
+
+      it 'remove shop from favorite with Hotwire' do
+        expect {
+          delete favorite_path(favorite)
+        }.to change(Favorite, :count).by(-1)
       end
     end
   end
