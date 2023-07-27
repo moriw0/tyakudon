2acc5c28a4d8ca9f69b544b1382fbf2680bc36da
shota <tontonka2@gmail.com>
2023-07-28 07:39:46 +0900

    refactoring

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,19 @@
 module UsersHelper
-  def avatar_for(user, options = { size: 32})
+  def avatar_for(user, variant_size)
     if user.avatar.attached?
-      size = options[:size]
-      image_tag user.avatar.variant(resize_to_fill: [size, size]), class: 'avatar'
+      case variant_size
+      when :small
+        image_tag user.avatar.variant(resize_to_limit: [33, 33]), class: 'small-avatar'
+      when :middle
+        image_tag user.avatar.variant(resize_to_limit: [80, 80]), class: 'avatar'
+      end
     else
-      image_tag 'blank_user_image.png', class: 'avatar'
+      case variant_size
+      when :small
+        image_tag 'blank_user_image.png', class: 'small-avatar'
+      when :middle
+        image_tag 'blank_user_image.png', class: 'avatar'
+      end
     end
   end
 
