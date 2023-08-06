90fa6afd2a015be65809cbffb5038c7fa2e95b61
shota <tontonka2@gmail.com>
2023-08-07 08:24:54 +0900

    add validations

--- app/models/ramen_shop.rb
+++ app/models/ramen_shop.rb
@@ -1,10 +1,15 @@
 class RamenShop < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :address_changed?
 
   has_many :records, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+
+  validates :name, presence: true, uniqueness: { scope: :address }
+  validates :address, presence: true
+  validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_blank: true
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_blank: true
 
   def self.ransackable_attributes(_auth_object = nil)
     ['name']
