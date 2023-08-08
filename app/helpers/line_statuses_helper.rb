39e9f2e0444536f24de7351efd8cf418feee9519
shota <tontonka2@gmail.com>
2023-08-08 13:04:47 +0900

    improve ramen shop index

--- app/helpers/line_statuses_helper.rb
+++ app/helpers/line_statuses_helper.rb
@@ -9,4 +9,17 @@ module LineStatusesHelper
       content_tag(:span, line_status.line_type_i18n, class: 'type-badge badge-red')
     end
   end
+
+  def status_badge_class(line_type)
+    case line_type
+    when 'inside_the_store'
+      'type-badge badge-yellow'
+    when 'outside_the_store'
+      'type-badge badge-red'
+    when 'seated'
+      'type-badge badge-green'
+    else
+      'bg-secondary'
+    end
+  end
 end
