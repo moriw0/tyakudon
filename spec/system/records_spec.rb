e4f57090f9e6019bedd726601fd8df50dd96e4a0
shota <tontonka2@gmail.com>
2023-08-16 08:10:50 +0900

    modify spec

--- spec/system/records_spec.rb
+++ spec/system/records_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe 'Records', js: true do
   end
 
   scenario 'user creates a record of the nearby ramen shop' do
-    click_link '現在地からセツゾク'
+    click_link '現在地から接続'
 
     # searchページ
-    expect(page).to_not have_link '現在地からセツゾク'
+    expect(page).to_not have_link '現在地から接続'
     expect(page).to have_css '.loading-spinner'
     expect(page).to have_css '#map', visible: :visible, wait: 15
     expect(page).to_not have_css '.loading-spinner'
@@ -22,23 +22,23 @@ RSpec.describe 'Records', js: true do
     find('label[for="record_line_statuses_attributes_0_line_type_outside_the_store"]').click
     fill_in '待ち行列数', with: 5
     fill_in 'ひとこと', with: '並ぶぞ'
-    click_button '登録する'
+    click_button '接続する'
 
     # measureページ
-    expect(page).to have_content 'セツゾクしました'
+    expect(page).to have_content '接続しました'
     expect(page).to have_content ramen_shop.name
     expect(page).to have_content '00:00:01', wait: 1
-    expect(page).to_not have_link '現在地からセツゾク'
+    expect(page).to_not have_link '現在地から接続'
 
     # measureページを一時離脱して再接続
     visit root_path
     record = Record.last
     click_link '接続中レコード', href: measure_record_path(record)
-    expect(page).to have_content '再セツゾクしました'
+    expect(page).to have_content '再接続しました'
     expect(page).to have_content '00:00:02', wait: 2
 
     # 待ち行列状況を追加登録
-    click_link '追加登録'
+    click_link '追加報告'
     find('label[for="line_status_line_type_outside_the_store"]').click
     fill_in '待ち行列数', with: 1
     ## 着席を選択すると行列数が0でhiddenされる
@@ -48,13 +48,13 @@ RSpec.describe 'Records', js: true do
     find('label[for="line_status_line_type_inside_the_store"]').click
     fill_in '待ち行列数', with: 1
     fill_in 'ひとこと', with: 'もう少し'
-    click_button '登録する'
+    click_button '報告する'
     ## 暫定対策: responseを待ってからmodalを強制的に閉じる
-    sleep(1)
-    find('button[data-bs-dismiss="modal"]').click
+    # sleep(1)
+    # find('button[data-bs-dismiss="modal"]').click
 
     # measureページに追加登録情報が反映されている
-    expect(page).to have_content '行列の様子を登録しました'
+    expect(page).to have_content '行列の様子を報告しました'
     expect(page).to have_content 'もう少し'
     expect(page).to have_content '00:00:05', wait: 3
     click_button 'ちゃくどん'
@@ -62,7 +62,7 @@ RSpec.describe 'Records', js: true do
     # 着丼後の投稿ページ
     expect(page).to have_content 'ちゃくどんレコードを登録しました'
     expect(page).to have_content '00:00:05'
-    fill_in '着丼してひとこと', with: '着丼しました'
+    fill_in 'ひとこと', with: '着丼しました'
     attach_file '写真', Rails.root.join('spec/fixtures/files/1000x800_4.2MB.png'), make_visible: true
     click_button '投稿する'
 
@@ -76,6 +76,6 @@ RSpec.describe 'Records', js: true do
     go_back
     go_back
     expect(page).to have_current_path(root_path)
-    expect(page).to have_link '現在地からセツゾク'
+    expect(page).to have_link '現在地から接続'
   end
 end
