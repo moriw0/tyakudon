ad89452c78b03a0d0033c290ac94b10fde33360d
shota <tontonka2@gmail.com>
2023-08-17 08:49:22 +0900

    move format helper

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -10,17 +10,4 @@ module RecordsHelper
   def fetch_record_id
     cookies.encrypted[:record_id]
   end
-
-  def format_datetime_detail(datetime)
-    return unless datetime
-
-    wdays = %w[日 月 火 水 木 金 土]
-    datetime.strftime("%Y.%m.%d(#{wdays[datetime.wday]}) %H:%M:%S")
-  end
-
-  def format_only_detatil_time(datetime)
-    return unless datetime
-
-    datetime.strftime("%H:%M:%S")
-  end
 end
