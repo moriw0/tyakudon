b5bf2a96a76deac9935b1dde68966244a88c556f
shota <tontonka2@gmail.com>
2023-09-23 14:21:13 +0900

    setting meta and ogp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,4 +57,37 @@ module ApplicationHelper
   def lazy_image_tag(image)
     image_tag image, loding: 'lazy', data: { image_target: 'image' }
   end
+
+  # rubocop:disable Metrics/MethodLength
+  def default_meta_tags
+    {
+      site: 'ちゃくどん',
+      title: 'ラーメン提供時間共有サービス',
+      reverse: true,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('favicon.ico'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      charset: 'utf-8',
+      description: '行列に並ぶ「接続」から、ラーメンが提供される「着丼」までの時間を計測して共有しよう',
+      keywords: 'ラーメン, 着丼',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@tonka2w0',
+        image: image_url('ogp.png')
+      }
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
 end
