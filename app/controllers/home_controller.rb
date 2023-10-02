b9db8bccbabb4bab9cd944c0372f7813bc09bf20
shota <tontonka2@gmail.com>
2023-10-02 21:49:21 +0900

    feed method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  include SessionsHelper
+
   before_action :logged_in_user, only: %i[search]
   before_action :set_search, only: %i[index record_ranking new_records]
   before_action :disable_connect_button, only: %i[search]
@@ -6,6 +8,7 @@ class HomeController < ApplicationController
   def index
     @ranking_records = Record.ranking_records.top_five
     @new_records = Record.new_records.top_five
+    @favorite_shops_records = current_user.records_from_favorite_shops if logged_in?
   end
 
   def record_ranking
