a928a53bef0b1bdfbab2a05e21a9e7c1fb823cb0
shota <tontonka2@gmail.com>
2023-11-25 05:44:49 +0900

    use turbostream to broadcast message

--- app/controllers/cheer_messages_controller.rb
+++ app/controllers/cheer_messages_controller.rb
@@ -2,19 +2,16 @@ class CheerMessagesController < ApplicationController
   def create
     record = Record.find(params[:id])
     current_wait_time = params[:current_wait_time]
-
     #実行時間をランダムに生成
     # random_wait_time = rand(1..3)
     #実行時間を元にjobを生成
     # SpeakMessageJob.set(wait: random_wait_time.seconds).perform_later(@post, data['content'])
+
     # 一旦websocketでメッセージを挿入
-    # message = post.messages.create(content: message)
-    # message.broadcast_prepend_to("messages")
+    message = record.cheer_messages.create(content: 'イケイケ応援している')
+    target = "cheer_message_#{message.id - 1}"
+    message.broadcast_replace_to('message', target: target)
 
-    # 純websocketでメッセージを出力
-    RecordChannel.broadcast_to(record, message: '応援しておる')
-
-    #jsonで返す
     if record
       render json: { id: record.id, waitTime: current_wait_time, message: '成功しました' }, status: :ok
     else
