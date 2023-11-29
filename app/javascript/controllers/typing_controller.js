21035cf81662812de9f376f006e4c864e7482e9c
shota <tontonka2@gmail.com>
2023-11-29 15:42:27 +0900

    typing effect

--- app/javascript/controllers/typing_controller.js
+++ app/javascript/controllers/typing_controller.js
@@ -3,8 +3,21 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="typing"
 export default class extends Controller {
   connect() {
-    setTimeout(() => {
-      this.element.remove();
-    }, 3000); //
+    this.element.removeAttribute("hidden");
+    this.typeMessage();
+  }
+
+  async typeMessage() {
+    const message = this.element.textContent;
+    this.element.textContent = "";
+
+    for (let i = 0; i < message.length; i++) {
+      this.element.textContent += message[i];
+      await new Promise((resolve) => setTimeout(resolve, 50));
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+
+    this.element.remove();
   }
 }
