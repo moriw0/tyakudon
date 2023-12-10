2f8950aaf3caf795b1967a6e340cb7aba6c4b06c
shota <tontonka2@gmail.com>
2023-12-11 06:39:40 +0900

    add like function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,18 @@
 class LikesController < ApplicationController
+  before_action :logged_in_user
+
   def create
+    @record = Record.find(params[:record_id])
+    current_user.add_like(@record)
   end
 
   def destroy
+    @record = Like.find(params[:id]).record
+    current_user.remove_like(@record)
+  end
+
+  def prepare
+    @record = Record.find(params[:id])
+    redirect_to record_path(@record)
   end
 end
