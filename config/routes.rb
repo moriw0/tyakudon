5de5c6116929afd1946c310653d2c53192886414
shota <tontonka2@gmail.com>
2023-12-10 16:04:46 +0900

    generate likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -36,5 +36,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :favorites, only: [:create, :destroy]
+  resources :likes, only: [:create, :destroy]
   resources :cheer_messages, only: %i[create]
 end
