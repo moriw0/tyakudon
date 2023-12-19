d67ec71d3f4ca540ebf4be7c333138cb08cbd8cf
shota <tontonka2@gmail.com>
2023-12-19 15:45:31 +0900

    use sitimulus to redirect prepare_like_path

--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -2,9 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="like"
 export default class extends Controller {
+  static values = { url: String };
+
   submit(event) {
     event.preventDefault();
     event.stopPropagation();
     this.element.requestSubmit();
+  }
+
+  redirect() {
+    window.location.href = this.urlValue;
   }
 }
