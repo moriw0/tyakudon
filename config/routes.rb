e264ec900187d76ae70f807ca4040caae8b0fd35
shota <tontonka2@gmail.com>
2024-01-08 14:27:05 +0900

    modify routes for root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'ranking_records#index'
+  root 'application#route_based_on_authentication'
   get '/lp', to: 'landing_page#index'
   get '/about', to: 'statics#about'
   get '/terms', to: 'statics#terms'
