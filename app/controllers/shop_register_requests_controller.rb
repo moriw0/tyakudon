f4467fd0e17438be85abd82cf90b92a35ef750c9
shota <tontonka2@gmail.com>
2024-01-18 15:15:18 +0900

    add new and create action

--- app/controllers/shop_register_requests_controller.rb
+++ app/controllers/shop_register_requests_controller.rb
@@ -1,7 +1,46 @@
 class ShopRegisterRequestsController < ApplicationController
+  before_action :logged_in_user
+
   def new
+    @request = ShopRegisterRequest.new
   end
 
   def create
+    @request = current_user.shop_register_requests.build(request_params)
+
+    if shop_exists?
+      handle_existing_shop
+    else
+      save_request
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:shop_register_request).permit(:name, :address, :remarks)
+  end
+
+  def update_request_params
+    params.require(:shop_register_request).permit(:name, :address, :remarks, :status)
+  end
+
+  def shop_exists?
+    RamenShop.exists?(name: @request.name, address: @request.address)
+  end
+
+  def handle_existing_shop
+    flash.now[:alert] = '店舗が既に存在します。'
+    @ramen_shop = RamenShop.find_by(name: @request.name, address: @request.address)
+    render :new
+  end
+
+  def save_request
+    if @request.save
+      @request.user.send_shop_register_request_email(@request)
+      redirect_to root_path, notice: 'リクエストを送信しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- spec/requests/shop_register_requests_spec.rb
+++ spec/requests/shop_register_requests_spec.rb
@@ -1,17 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe 'ShopRegisterRequests' do
+  let!(:user) { create(:user) }
+
+  before do
+    log_in_as user
+  end
+
   describe 'GET /new' do
     it 'returns http success' do
-      get '/shop_register_requests/new'
+      get new_shop_register_request_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/shop_register_requests/create'
-      expect(response).to have_http_status(:success)
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new ShopRegisterRequest' do
+        expect {
+          post shop_register_requests_path,
+               params: { shop_register_request: { name: 'New Ramen Shop', address: '123 Ramen Street',
+                                                  remarks: 'Great place!' } }
+        }.to change(ShopRegisterRequest, :count).by(1)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new ShopRegisterRequest' do
+        expect {
+          post shop_register_requests_path, params: { shop_register_request: { name: '', address: '' } }
+        }.to_not change(ShopRegisterRequest, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'when shop already exists' do
+      before do
+        create(:ramen_shop, name: 'Existing Ramen Shop', address: '123 Ramen Street')
+      end
+
+      it 'does not create a new ShopRegisterRequest' do
+        post shop_register_requests_path,
+             params: { shop_register_request: { name: 'Existing Ramen Shop', address: '123 Ramen Street' } }
+        expect(ShopRegisterRequest.count).to eq(0)
+        expect(flash[:alert]).to eq '店舗が既に存在します。'
+      end
     end
   end
 end
