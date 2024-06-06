209af9163943be63b162ba64f3427e8cebebdc54
shota <tontonka2@gmail.com>
2024-06-06 09:03:08 +0900

    setting for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://e54ad48a6ce26bbb08d0c1f99c6098fd@o4506884486397952.ingest.us.sentry.io/4506884486594560'
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # To activate performance monitoring, set one of these options.
@@ -9,4 +9,7 @@ Sentry.init do |config|
   config.traces_sampler = lambda do |context|
     true
   end
+
+  config.environment = Rails.env
+  config.enabled_environments = %w[production]
 end
