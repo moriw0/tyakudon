725b7b9454481a4c58f7e42dc8d83b6f61ffff50
Shota Morinaga <shota.morinaga@as-child.com>
2026-01-25 07:52:57 +0900

    Revert "show num of users and records on lp"
    This reverts commit 22a2e5dd3bc5a68db0ec61358ab485101bd7144e.
    
    コンフリクトも解消

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,15 +2,10 @@ class LandingPageController < ApplicationController
   before_action :disable_connect_button
   layout 'lp'
 
-  # rubocop:disable Metrics/AbcSize
   def index
-    @today = Time.zone.today.strftime('%-m月%-d日')
-    @users_count = User.all.count
-    @records_count = Record.where(is_test: false).count
     @longest_record = Record.not_retired.with_associations.order_by_longest_wait.first
     @most_like_record = Record.not_retired.with_associations.order_by_most_likes.first
     @shortest_record = Record.not_retired.with_associations.order_by_shortest_wait.first
     @faqs = Faq.all.limit(5)
   end
-  # rubocop:enable Metrics/AbcSize
 end
