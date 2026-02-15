e49af17d653f699b8b4f5a67c77d9a11e776aaf8
Shota Morinaga <shota.morinaga@as-child.com>
2026-02-15 14:06:19 +0900

    feat: sort RamenShops#index by records count descending

--- app/controllers/ramen_shops_controller.rb
+++ app/controllers/ramen_shops_controller.rb
@@ -6,7 +6,7 @@ class RamenShopsController < ApplicationController
   def index
     @search = RamenShop.search_by_keywords(params[:q])
     @result_count = @search.result.count
-    @ramen_shops = @search.result.with_associations.page(params[:page])
+    @ramen_shops = @search.result.with_associations.order_by_records_count.page(params[:page])
 
     respond_to do |format|
       format.html
