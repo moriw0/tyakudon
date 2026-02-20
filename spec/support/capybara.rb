e8decc502917c35351c7ebd06c6b88ac7adc95de
Shota Morinaga <shota.morinaga@as-child.com>
2026-02-21 01:21:29 +0900

    test: Docker環境でSeleniumリモートドライバーを使用するよう設定
    - compose.yaml: seleniarm/standalone-chromiumサービスを追加
      - ARM64(Apple Silicon)向けにseleniarmイメージを使用
      - SELENIUM_IMAGE環境変数でx86_64イメージへの切り替えに対応
      - SELENIUM_REMOTE_URL環境変数をappサービスに追加
    - spec/support/capybara.rb: リモートSeleniumに対応
      - SELENIUM_REMOTE_URL設定時はremoteドライバーを使用
      - Capybara.server_host/portを固定化しDocker間通信を確立
      - IPアドレス取得失敗時の明示的エラーを追加
      - 非JSテスト後のapp_hostリセット処理を追加
      - --headlessを--headless=newに更新(Chrome 112+対応)

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,17 +1,40 @@
+require 'socket'
+
+Capybara.server_host = '0.0.0.0'
+# Fixed port required for remote Selenium to connect back to the Capybara server.
+Capybara.server_port = 3001
+
 Capybara.register_driver :selenium_chrome_custom do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
+  options.add_argument('--headless=new')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+
+  if ENV['SELENIUM_REMOTE_URL'].present?
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :remote,
+      url: ENV['SELENIUM_REMOTE_URL'],
+      options: options
+    )
+  else
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
 end
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
+    Capybara.app_host = nil
     driven_by :rack_test
   end
 
   config.before(:each, js: true, type: :system) do
+    if ENV['SELENIUM_REMOTE_URL'].present?
+      ip = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
+      raise 'No private IPv4 address found. Cannot connect Selenium to the Capybara server.' unless ip
+
+      Capybara.app_host = "http://#{ip}:#{Capybara.server_port}"
+    end
     driven_by :selenium_chrome_custom
   end
 end
