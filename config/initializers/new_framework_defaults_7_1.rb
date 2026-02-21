be2152ec19404d2864a023abfe6ec7c2dddf421f
Shota Morinaga <shota.morinaga@as-child.com>
2026-02-21 17:22:11 +0900

    chore: action_text.sanitizer_vendor を有効化

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -261,7 +261,7 @@ Rails.application.config.active_record.encryption.support_sha1_for_non_determini
 #
 # In previous versions of Rails, Action Text always used `Rails::HTML4::Sanitizer` as its vendor.
 #++
-# Rails.application.config.action_text.sanitizer_vendor = Rails::HTML::Sanitizer.best_supported_vendor
+Rails.application.config.action_text.sanitizer_vendor = Rails::HTML::Sanitizer.best_supported_vendor
 
 ###
 # Configure the log level used by the DebugExceptions middleware when logging
