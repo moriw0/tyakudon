841026af64346d746e27193a4962c73163f1e1ec
Shota Morinaga <shota.morinaga@as-child.com>
2026-02-21 17:09:05 +0900

    chore: precompile_filter_parameters を有効化

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -181,7 +181,7 @@ Rails.application.config.active_record.encryption.support_sha1_for_non_determini
 # Enable precompilation of `config.filter_parameters`. Precompilation can
 # improve filtering performance, depending on the quantity and types of filters.
 #++
-# Rails.application.config.precompile_filter_parameters = true
+Rails.application.config.precompile_filter_parameters = true
 
 ###
 # Enable before_committed! callbacks on all enrolled records in a transaction.
