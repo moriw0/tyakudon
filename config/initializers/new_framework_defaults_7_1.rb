5129dc4a833c213d40ca905680f4f88966e7b63e
Shota Morinaga <shota.morinaga@as-child.com>
2026-02-21 17:16:13 +0900

    chore: default_column_serializer を有効化

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -196,7 +196,7 @@ Rails.application.config.active_record.encryption.support_sha1_for_non_determini
 # recommended to explicitly define the serialization method for each column
 # rather than to rely on a global default.
 #++
-# Rails.application.config.active_record.default_column_serializer = nil
+Rails.application.config.active_record.default_column_serializer = nil
 
 ###
 # Enable a performance optimization that serializes Active Record models
