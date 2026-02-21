0111aec73716f3f8a24ba4cf3149a08393ccbb6f
Shota Morinaga <shota.morinaga@as-child.com>
2026-02-21 17:02:37 +0900

    chore: debug_exception_log_level を有効化

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -267,7 +267,7 @@ Rails.application.config.active_record.encryption.support_sha1_for_non_determini
 # Configure the log level used by the DebugExceptions middleware when logging
 # uncaught exceptions during requests.
 #++
-# Rails.application.config.action_dispatch.debug_exception_log_level = :error
+Rails.application.config.action_dispatch.debug_exception_log_level = :error
 
 ###
 # Configure the test helpers in Action View, Action Dispatch, and rails-dom-testing to use HTML5
