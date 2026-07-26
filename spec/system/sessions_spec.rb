6d7e68f3bda3e40961a20a18346b98b85a840fc9
Shota Morinaga <shota.morinaga@as-child.com>
2026-07-27 08:29:36 +0900

    test: system spec に v2 opt-in ヘルパーを追加しログイン/ログアウトをv2化
    CONTEXT.md: 接続 / 着丼などの用語に変更なし。
    
    Closes #340

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Logins' do
   let(:user) { create(:user) }
 
-  before { create(:record, :with_line_status, user: user) }
+  before { enable_v2_ui }
 
   it 'login with valid email and invalid password' do
     visit login_path
@@ -21,11 +21,12 @@ RSpec.describe 'Logins' do
     fill_in 'パスワード', with: user.password
     click_button 'ログインする'
     expect(page).to have_content 'ログインしました'
-    expect(page).to have_link 'プロフィール', href: user_path(user)
-    expect(page).to have_link 'ログアウト', href: logout_path
-    click_link 'ログアウト'
+    expect(page).to have_link user.name, href: user_path(user)
+    click_link user.name
+    expect(page).to have_link 'ログアウトする', href: logout_path
+    click_link 'ログアウトする'
     expect(page).to have_content 'ログアウトしました'
     expect(page).to have_link 'ログイン', href: login_path
-    expect(page).to_not have_link 'ログアウト', href: logout_path
+    expect(page).to_not have_link user.name, href: user_path(user)
   end
 end
